ff7c3fdff5ae9c99c080f2d97f6739b0006c43b5
David Barcenas <daveepro@outlook.com>
2021-10-18 21:58:04 -0500

    configure mysql database

--- store-rest-api/store/settings.py
+++ store-rest-api/store/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import environ
 from pathlib import Path
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)ok0y_5fvs(48twus29ols!9ems6@vb83e5oa2*r7*v*on#snb'
+SECRET_KEY = env.db('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env.db('DATABASE_NAME'),
+        'USER': env.db('DATABASE_USER'),
+        'PASSWORD': env.db('DATABASE_PASSWORD'),
+        'HOST': env.db('DATABASE_HOST'),
+        'PORT': env.db('DATABASE_PORT'),
     }
 }
 
