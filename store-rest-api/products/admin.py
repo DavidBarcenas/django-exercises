df9bc1260458478696f0b4263d2d76aee686e8ac
David Barcenas <daveepro@outlook.com>
2021-10-19 16:00:32 -0500

    register models in admin

--- store-rest-api/products/admin.py
+++ store-rest-api/products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Type, Product
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+
+
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- store-rest-api/products/models.py
+++ store-rest-api/products/models.py
@@ -5,10 +5,16 @@ class Category(models.Model):
     title = models.CharField(max_length=255)
     url_clean = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.title
+
 
 class Type(models.Model):
     title = models.CharField(max_length=255)
     url_clean = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
 
 
 class Product(models.Model):
@@ -17,3 +23,6 @@ class Product(models.Model):
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     type = models.ForeignKey(Type, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
