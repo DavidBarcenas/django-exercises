1f58aa61754045f4fce2a46a3be52c21c7fb4bd4
David Barcenas <daveepro@outlook.com>
2021-10-19 18:19:26 -0500

    create category and type serializers

--- store-rest-api/products/urls.py
+++ store-rest-api/products/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import ProductViewSet
+from .views import CategoryViewSet, ProductViewSet, TypeViewSet
 
 router = routers.SimpleRouter()
 router.register('products', ProductViewSet)
+router.register('category', CategoryViewSet)
+router.register('type', TypeViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
--- store-rest-api/products/views.py
+++ store-rest-api/products/views.py
@@ -1,9 +1,22 @@
+from django.db.models import query
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Category, Product, Type
+from .serializers import CategorySerializer, ProductSerializer, TypeSerializer
+
 
 class ProductViewSet(viewsets.ModelViewSet):
-  queryset = Product.objects.all()
-  serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class TypeViewSet(viewsets.ModelViewSet):
+    queryset = Type.objects.all()
+    serializer_class = TypeSerializer
