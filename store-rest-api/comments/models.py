a2fc5beae86c2e6888249d61fc890aec6f0a67e9
David Barcenas <daveepro@outlook.com>
2021-10-20 17:23:50 -0500

    create model and register in admin

--- store-rest-api/comments/models.py
+++ store-rest-api/comments/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    text = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return 'Comment #{}'.format(self.id)
--- store-rest-api/store/settings.py
+++ store-rest-api/store/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "corsheaders",
     "products",
+    "comments"
 ]
 
 MIDDLEWARE = [
