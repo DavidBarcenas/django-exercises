c8824ec53f316cb47ca9fa8a19115af4ee7fa3ff
David Barcenas <daveepro@outlook.com>
2021-10-20 18:51:18 -0500

    validate form and redirect path

--- store-rest-api/comments/views.py
+++ store-rest-api/comments/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from comments.models import Comment
 from comments.forms import CommentForm
@@ -10,5 +10,15 @@ def index(req):
 
 
 def add(req):
+
+    if(req.method == 'POST'):
+        form = CommentForm(req.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('comments:index')
+    else:
+        form = CommentForm()
+
     form = CommentForm
     return render(req, 'add.html', {'form': form})
