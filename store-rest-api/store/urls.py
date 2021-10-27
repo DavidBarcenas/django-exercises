ca5950dd5233ca0e3d66f4e7b62ce779c3fa9a6e
David Barcenas <daveepro@outlook.com>
2021-10-27 14:38:54 -0500

    show image in client view

--- store-rest-api/store/urls.py
+++ store-rest-api/store/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,9 @@ urlpatterns = [
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
