066eea92e722b56ce5f4b6126a1f608f099271a2
David Barcenas <daveepro@outlook.com>
2021-11-06 19:31:12 -0600

    filter by title and category

--- store-rest-api/shop/views.py
+++ store-rest-api/shop/views.py
@@ -2,17 +2,31 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.views import generic
 
-from products.models import Product
+from products.models import Category, Product
 
 
 def index(req):
-    products = Product.objects.all()
+    categories = Category.objects.all()
+
+    search = req.GET.get('search')
+    category = req.GET.get('category')
+
+    if search:
+        products = Product.objects.filter(title__contains=search)
+    else:
+        products = Product.objects.all()
+
+    if category:
+        products = products.objects.filter(category_id__contains=category)
 
     paginator = Paginator(products, 5)
     page_number = req.GET.get('page')
     products_page = paginator.get_page(page_number)
 
-    return render(req, 'store/index.html', {'products': products_page})
+    return render(req, 'store/index.html', {
+        'products': products_page,
+        'categories': categories
+    })
 
 
 class DetailView(generic.DetailView):
