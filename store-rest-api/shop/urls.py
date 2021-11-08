ddecaccacbc35c36c5ecc5d0de13bd29581dd3bf
David Barcenas <daveepro@outlook.com>
2021-11-08 15:37:41 -0600

    set up paypal payment method

--- store-rest-api/shop/urls.py
+++ store-rest-api/shop/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('product/payment', views.make_pay_paypal, name='payment'),
     path('product/<int:pk>', views.DetailView.as_view(), name='detail'),
     path('product/<slug:url_clean>', views.DetailView.as_view(), name='detail'),
 ]
--- store-rest-api/shop/views.py
+++ store-rest-api/shop/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.views import generic
+import paypalrestsdk
+import logging
+import environ
 
 from products.models import Category, Product
+
+env = environ.Env()
 
 
 def index(req):
@@ -39,3 +44,43 @@ class DetailView(generic.DetailView):
     template_name = 'store/detail.html'
     slug_field = 'url_clean'
     slug_url_kwarg = 'url_clean'
+
+
+def make_pay_paypal(req):
+    paypalrestsdk.configure({
+        "mode": "sandbox",
+        "client_id": env("PAYPAL_CLIENT_ID"),
+        "client_secret": env("PAYPAL_CLIENT_SECRET")
+    })
+
+    payment = paypalrestsdk.Payment({
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"},
+        "redirect_urls": {
+            "return_url": "http://localhost:3000/payment/execute",
+            "cancel_url": "http://localhost:3000/"},
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "item",
+                    "sku": "item",
+                    "price": "5.00",
+                    "currency": "USD",
+                    "quantity": 1}]},
+            "amount": {
+                "total": "5.00",
+                "currency": "USD"},
+            "description": "This is the payment transaction description."}]})
+
+    if payment.create():
+        print("Payment created successfully")
+    else:
+        print(payment.error)
+
+    for link in payment.links:
+        if link.rel == "approval_url":
+            # Convert to str to avoid Google App Engine Unicode issue
+            # https://github.com/paypal/rest-api-sdk-python/pull/58
+            approval_url = str(link.href)
+            print("Redirect for approval: %s" % (approval_url))
