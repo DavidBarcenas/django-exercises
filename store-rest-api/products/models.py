d7329cc8aee37a0c038908bb8f14741c9eef4f28
David Barcenas <daveepro@outlook.com>
2021-11-08 21:41:47 -0600

    add price to product model

--- store-rest-api/products/models.py
+++ store-rest-api/products/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     url_clean = models.CharField(max_length=255)
     description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     type = models.ForeignKey(Type, on_delete=models.CASCADE)
 
