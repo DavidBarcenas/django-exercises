3bf6e289bef37812fb5fef84eab44269d28835fe
David Barcenas <daveepro@outlook.com>
2021-11-09 21:35:52 -0600

    create payment model

--- store-rest-api/shop/models.py
+++ store-rest-api/shop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from products.models import Product
+
+
+class Payment(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    payment_id = models.CharField(max_length=200)
+    payer_id = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.price
